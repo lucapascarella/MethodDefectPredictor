2cb2d7ae8973422670037e6b71e3dd3b9600bcd6
Luca Pascarella <luca.pascarella@gmail.com>
2019-07-22 15:47:19 +0200

    Speed up bic.py

--- bic.py
+++ bic.py
@@ -1,7 +1,9 @@
 import argparse, csv
 import os
+from pydriller import Commit
 
 from pydriller import GitRepository
+from pydriller.domain.commit import Modification
 
 
 def get_method_count(modifications):
@@ -18,12 +20,19 @@ def get_bic_count(bic_mods):
     return count
 
 
+def get_fix_mod_by_path(commit: Commit, bic_path: str) -> Modification:
+    for mod in commit.modifications:
+        if mod.new_path == bic_path or mod.old_path == bic_path:
+            return mod
+    return None
+
+
 # Main
 if __name__ == '__main__':
     print("*** BIC started ***\n")
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--repo', type=str, help='Local absolute or relative path to a valid GIT repository.', default=None)
-    parser.add_argument('-i', '--input', type=str, help='A CSV file that contains a list of commit fixes, the fix must be identified with a valid HASH belonging to the -r or --repo otion', default='data/fix_commits.csv')
+    parser.add_argument('-c', '--csv', type=str, help='A CSV file that contains a list of commit fixes, the fix must be identified with a valid HASH belonging to the -r or --repo otion', default='data/fix_commits.csv')
     parser.add_argument('-n', '--notuse', type=str, help='Absolute path of a text file that contains a list of commits, one per line, to ignore', default=None)  # 'data/ignore_commits.txt'
     parser.add_argument('-d', '--delimiter', type=str, help='The CSV delimiter', default=',')
     parser.add_argument('-f', '--fix', type=str, help='The name of the column in the input CSV file used to identify the GIT HASH of a fix commit.', default='git_hash')
@@ -35,56 +44,71 @@ if __name__ == '__main__':
         print('A valid path to a local GIT repository must be specified!')
         exit(-1)
     # Check that a valid input is specified
-    if not os.path.isfile(args.input):
+    if not os.path.isfile(args.csv):
         print('A valid path to an input (CSV file) must be given as input!')
         exit(-1)
     # Check that a valid ignore file is specified
     if args.notuse is not None:
         if not os.path.isfile(args.notuse):
-            print('A valid path to a input text must be given as input!')
+            print('A valid path to an input text with the list of commits to ignore must be given as input!')
             exit(-1)
 
     # Read the column names of the input file
-    input_file = open(args.input, 'r', encoding="utf-8")
+    input_file = open(args.csv, 'r', encoding="utf-8")
     header = input_file.readline()
     input_columns = header.strip().split(args.delimiter)
 
+    # Read commits to ignore
+    ignore_file = open(args.notuse, 'r', encoding="utf-8")
+    ignore_commits = ignore_file.readlines()
+    ignore_file.close()
+
     # Prepare the column names for the output file
-    header = input_columns + ['git_timestamp', 'git_modifications', 'git_methods', 'bic_count', 'bic_commit', 'bic_timestamp', 'bic_modifications', 'bic_methods']
+    # header = input_columns + ['git_timestamp', 'git_modifications', 'git_methods', 'bic_count', 'bic_commit', 'bic_timestamp', 'bic_modifications', 'bic_methods']
+    header = input_columns + ['git_timestamp', 'git_modifications', 'git_methods', 'fix_added', 'fix_removed', 'bic_count', 'bic_commit', 'bic_path', 'bic_timestamp', 'bic_modifications']
     out_file = open(args.output, 'w', newline='', encoding="utf-8")
     writer = csv.DictWriter(out_file, delimiter=args.delimiter, fieldnames=header)
     writer.writeheader()
 
     # Perform Git blame to retrieve the list of BIC commits
     gr = GitRepository(args.repo)
-    fixes = csv.DictReader(open(args.input, 'r', newline='', encoding="utf-8"), delimiter=args.delimiter)
+    fixes = csv.DictReader(open(args.csv, 'r', newline='', encoding="utf-8"), delimiter=args.delimiter)
     count = 0
     for fix in fixes:
         git_hash = fix['git_hash']
         print('{}) Processing {} '.format(count, git_hash))
         fix_commit = gr.get_commit(git_hash)
-        if args.notuse:
-            bic_mods = gr.get_commits_last_modified_lines(fix_commit, hashes_to_ignore_path=args.notuse)
-        else:
-            bic_mods = gr.get_commits_last_modified_lines(fix_commit)
-        print('{}) {} has {} MOD, {} BIC'.format(count, git_hash, len(bic_mods), get_bic_count(bic_mods)))
+        for mod in fix_commit.modifications:
+            if mod.filename.endswith('.cpp'):
+                if args.notuse:
+                    bic_mods = gr.get_commits_last_modified_lines(fix_commit, mod, hashes_to_ignore_path=args.notuse)
+                else:
+                    bic_mods = gr.get_commits_last_modified_lines(fix_commit, mod)
+                print('{}) {} has {} MOD, {} BIC'.format(count, git_hash, len(bic_mods), get_bic_count(bic_mods)))
 
-        dout = {'git_timestamp': fix_commit.committer_date,
-                'git_modifications': len(fix_commit.modifications),
-                'git_methods': get_method_count(fix_commit.modifications),
-                'bic_count': len(bic_mods)}
-        # Append the ancillary data contained in the input file
-        for ic in input_columns:
-            dout[ic] = fix[ic]
+                dout = {'git_timestamp': fix_commit.committer_date,
+                        'git_modifications': len(fix_commit.modifications),
+                        'git_methods': get_method_count(fix_commit.modifications),
+                        'bic_count': len(bic_mods)}
+                # Append the ancillary data contained in the input file
+                for ic in input_columns:
+                    dout[ic] = fix[ic]
 
-        for mod, bic_commit_hashs in bic_mods.items():
-            for bic_commit_hash in bic_commit_hashs:
-                bic_commit = gr.get_commit(bic_commit_hash)
-                dout['bic_commit'] = bic_commit_hash
-                dout['bic_timestamp'] = bic_commit.committer_date
-                dout['bic_modifications'] = len(bic_commit.modifications)
-                dout['bic_methods'] = get_method_count(bic_commit.modifications)
-                writer.writerow(dout)
+                for bic_path, bic_commit_hashs in bic_mods.items():
+                    fix_mod = get_fix_mod_by_path(fix_commit, bic_path)
+                    if fix_mod is not None:
+                        dout['fix_added'] = fix_mod.added
+                        dout['fix_removed'] = fix_mod.removed
+
+                    for bic_commit_hash in bic_commit_hashs:
+                        if bic_commit_hash not in ignore_commits:
+                            bic_commit = gr.get_commit(bic_commit_hash)
+                            dout['bic_commit'] = bic_commit_hash
+                            dout['bic_path'] = bic_path
+                            dout['bic_timestamp'] = bic_commit.committer_date
+                            dout['bic_modifications'] = len(bic_commit.modifications)
+                            # dout['bic_methods'] = get_method_count(bic_commit.modifications)
+                            writer.writerow(dout)
         count += 1
         out_file.flush()
     out_file.close()
